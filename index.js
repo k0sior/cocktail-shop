2cb1a5c6d487b2d8e823329022a90c26c3bda709
k0sior <mac.kosiorek@gmail.com>
2021-01-04 15:41:28 +0100

    fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './src/App';
 // import reportWebVitals from './reportWebVitals';
-import { AppProvider } from './context'
+import { AppProvider } from './src/context'
 
 ReactDOM.render(
   <React.StrictMode>
